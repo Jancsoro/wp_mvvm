acedd9fec6363febee3e16aefc8fb02dde28f8ba
Jancsoro <fjanos9201@gmail.com>
2016-02-29 00:02:16 +0100

    Views added
    Views added, but some problems in the bindings, or some wp stuff

---
+++ mvvmtestapp/Veiw/LevelView.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using Windows.UI.Xaml.Controls;
+using System.Windows;
+using Windows.UI.Xaml.Data;
+
+namespace MVVMTestApp.View
+{
+    public partial class LevelView : UserControl
+    {
+        public LevelView()
+        {
+            InitializeComponent();
+        }
+    }
+
+
+    public class BoolOpposite : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            bool b = (bool)value;
+            return !b;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            string s = value as string;
+            bool b;
+
+            if (bool.TryParse(s, out b))
+            {
+                return !b;
+            }
+            return false;
+        }
+    }
+}
