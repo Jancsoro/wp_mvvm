d92a75ef234fbeb8b996de7cbf12c7a5b10c1734
Jancsoro <fjanos9201@gmail.com>
2016-02-28 23:00:05 +0100

    model added
    wp project and mvvm model added

---
+++ mvvmtestapp/Model/Accomplishment.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mvvmtestapp.Model
+{
+    class Accomplishment : INotifyPropertyChanged
+    {
+        public String name { get; set; }
+        public String type { get; set; }
+        private int _count;
+        public int Count
+        {
+            get
+            {
+                return _count;
+            }
+            set
+            {
+                _count = value;
+                RaisePropertyChanged("Count");
+            }
+        }
+
+        private bool _completed;
+        public bool Completed
+        {
+            get
+            {
+                return _completed;
+            }
+            set
+            {
+                _completed = value;
+                RaisePropertyChanged("Completed");
+            }
+        }
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Accomplishment GetCopy()
+        {
+            Accomplishment copy = (Accomplishment)this.MemberwiseClone();
+            return copy;
+        }
+    }
+}
